7e08a4b8f866800886a8da0a017b9e2e4ef2e407
forfd8960 <forfd8960@gmail.com>
2025-09-11 15:46:25 +0800

    [feat] init mods and adding dependency

---
+++ src/config/mod.rs
@@ -0,0 +1,5 @@
+#[derive(Debug)]
+pub struct CacheConfig {
+    pub addr: String,
+    pub port: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod commands; // handle command, SET, GET, ZADD, etc
+mod config; // handle server config.
+mod network; // handle network connection handler.
+mod persistence; // data persistence.
+mod protocol; // redis protocol decode and encode.
+mod storage; // data store
+mod utils; // util functions.
+
 fn main() {
     println!("A Redis Server build with Rust");
 }
